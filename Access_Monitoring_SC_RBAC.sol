95b45460f7f4909704631a9b7a9167e017232c0b
lucas-frosdick <62063613+lucas-frosdick@users.noreply.github.com>
2023-09-02 11:32:00 +0100

    Updated Access Monitoring Smart Contract

--- Access_Monitoring_SC_RBAC.sol
+++ Access_Monitoring_SC_RBAC.sol
@@ -2,17 +2,16 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
-//import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 /*Access Monitoring with RBAC smart contract*/
-contract AM_SC_RBAC is AccessControl//, Ownable
+contract AM_SC_RBAC is AccessControl
 {
 
     mapping (bytes32 => Role_Struct) RBAC_Roles; //stores the user roles required for RBAC implementation
     mapping (address => User_Struct) Users_Info; //stores the user profiles required for RBAC implementation
     mapping (bytes32 => System_Struct) Systems;  //stores the systems availabe to users and the roles required to access them
-    mapping (address => User_RBAC_Session) User_Sessions;
+    mapping (address => User_RBAC_Session) User_Sessions; //stores the RBAC sessions created by the user
 
     bytes32[] Roles; //array of role hashes
     address[] Users; //array of user addresses
@@ -101,7 +100,6 @@ contract AM_SC_RBAC is AccessControl//, Ownable
     //format of the system information
     struct System_Struct
     {
-        //mapping (uint256 => Role_Struct) Required_Role_Identifier;
         bytes32[] Required_Role_Hash;
         uint System_Role_Count;
         mapping (bytes32 => System_Resources) System_Resources;
@@ -138,10 +136,6 @@ contract AM_SC_RBAC is AccessControl//, Ownable
         bytes32 Role_Hash = keccak256(abi.encodePacked(Role));  //generate keccak256 hash of role
         require(RBAC_Roles[Role_Hash].Role_Timestamp == 0, "Role Exists");  //ensure that the role does not already exist
 
-        //bytes32[] memory Resource_Hash = new bytes32[](Resources.length);
-
-
-
         //initalise the role structure and store it within RBAC_Roles
         RBAC_Roles[Role_Hash].Role_Name = Role;
         RBAC_Roles[Role_Hash].Role_Information = Info;
@@ -152,9 +146,7 @@ contract AM_SC_RBAC is AccessControl//, Ownable
         for (uint256 i = 0; i < Resources.length; i++) 
         {
             bytes32 TMP_Hash = keccak256(abi.encodePacked(Resources[i]));   // Generate hash of the resource name
-
             RBAC_Roles[Role_Hash].Allowed_Resources[TMP_Hash].Resource_Name = Resources[i]; // Set resource name in role
-
             require(Resource_Permissions[i] < 4 && Resource_Permissions[i] > 0, "Resource permission incorrect"); // ensure permissions are between 1 and 3 (inclusive)
 
             if (Resource_Permissions[i] == 1)   // if role permission for resource is to allow reads
@@ -181,8 +173,6 @@ contract AM_SC_RBAC is AccessControl//, Ownable
         }
 
         RBAC_Roles[Role_Hash].Allowed_Resources_Count = Resources.length;   // store number of resources allowed by the role
-        
- 
         Index_Role++; //increment number of roles
         Roles.push(Role_Hash); //store the generated hash of the role
     }
@@ -196,7 +186,7 @@ contract AM_SC_RBAC is AccessControl//, Ownable
         require(RBAC_Roles[Role_Hash].Role_Timestamp != 0, "Role Does Not Exists"); //ensure that the role exists
 
         delete RBAC_Roles[Role_Hash]; //delete role from RBAC_Roles
-        //Index_Role--; //decrement number of roles
+        Index_Role--; //decrement number of roles
     }
 
     /*add user to the smart contract, takes the address of the user, the role to be assigned to the user, and information about the user*/
@@ -215,7 +205,6 @@ contract AM_SC_RBAC is AccessControl//, Ownable
         Users_Info[User_Address].User_Joined = block.timestamp;
         Users_Info[User_Address].User_Index = Index_User;
 
-        
         Index_User++;   //increment the number of users in the system
         Users.push(User_Address);   //store the user address hash
         grantRole(Role_Hash, User_Address); //utilise Openzeplin's AccessControl to grant user role
@@ -228,16 +217,13 @@ contract AM_SC_RBAC is AccessControl//, Ownable
         bytes32 Role_Hash = keccak256(abi.encodePacked(Role));
         require(RBAC_Roles[Role_Hash].Role_Timestamp != 0, "Role Does Not Exists"); // ensure the role exists
 
-
         for (uint256 i = 0; i < Users_Info[User_Address].User_Role_Count; i++) //   for each user role
         {
             require(Users_Info[User_Address].User_Role_Hash[i] != Role_Hash, "User Already Assigned this Role");    //  ensure user is not already assigned the role
         }
         
-        
         Users_Info[User_Address].User_Role_Hash.push(Role_Hash);    // store role against the user
         Users_Info[User_Address].User_Role_Count++;     //  increment the number of roles assigned to the user
-
         grantRole(Role_Hash, User_Address); // grant the user the role
     }
 
@@ -263,10 +249,10 @@ contract AM_SC_RBAC is AccessControl//, Ownable
         }
 
         require(Role_Found, "User not assigned to role");   //  ensure that the role has been found
+        revokeRole(Users_Info[User_Address].User_Role_Hash[i], User_Address);   //  revoke role from user
         delete Users_Info[User_Address].User_Role_Hash[i];  //  remove role from users roles
 
-        //Users_Info[User_Address].User_Role_Count--;
-        
+        Users_Info[User_Address].User_Role_Count--;        
     }
 
     /*remove user from the smart contract, takes the address of the user*/
@@ -281,10 +267,8 @@ contract AM_SC_RBAC is AccessControl//, Ownable
             revokeRole(User_Role, User_Address);    //utilise Openzepplins AccessControl to revoke the users role
         }
         
-        
-
         delete Users_Info[User_Address];    //delete user from User_Info
-        //Index_User--;   //decrement the number of users
+        Index_User--;   //decrement the number of users
     }
 
     /*Configure system that users can access, takes the name of the system, the role required to access the system, and information about the system*/
@@ -325,7 +309,7 @@ contract AM_SC_RBAC is AccessControl//, Ownable
         require(Systems[System_Hash].System_Added != 0, "System Does Not Exists");  //ensure that the system exists
 
         delete Systems[System_Hash];    //delete system
-        //Index_System--; //decrement number of systems
+        Index_System--; //decrement number of systems
     }
 
 
@@ -361,14 +345,12 @@ contract AM_SC_RBAC is AccessControl//, Ownable
     {
         Access = false; //assume that user cannot access system until the opposite is proven
         bool role = false;
-        address User_Add = msg.sender;
+        address User_Add = msg.sender;  //retreive the address of the user
         bytes32 Role_Test;
 
         require(Users_Info[User_Add].User_Joined != 0, "User Does Not Exist"); //ensure that the user exists within the system
         bytes32 System_Hash = keccak256(abi.encodePacked(Access_Request));  //generate system name hash of the requested system
         require(Systems[System_Hash].System_Added != 0, "Requested System Does Not Exist"); //ensure requested system exists
-        //bytes32 Required_Role = Systems[System_Hash].Required_Role_Identifier;  //fetch the role required to access the system
-
         bytes32[] memory Role_Hash = new bytes32[](Systems[System_Hash].System_Role_Count);
 
         if (Users_Info[User_Add].Monitor_Behavior.Resource_Request_Fail)
@@ -377,12 +359,10 @@ contract AM_SC_RBAC is AccessControl//, Ownable
             Users_Info[User_Add].Monitor_Behavior.Resource_Request_Fail = false;
         }       
        
-       
         if (Users_Info[User_Add].Monitor_Behavior.User_Restricted == true)  // check if the users access is restricted
         {
             return Access;  // User cannot access the system
         }
-
 
         for (uint i = 0; i< Systems[System_Hash].System_Role_Count; i++) 
         {
@@ -390,7 +370,16 @@ contract AM_SC_RBAC is AccessControl//, Ownable
             role = hasRole(Role_Test, User_Add); //utilise Openzepplins AccessControl to ensure user has required role
             if (role)
             {
-                Role_Hash[i] = Role_Test;
+                // If the role exists
+                if (RBAC_Roles[Role_Test].Role_Timestamp != 0)
+                {
+                    Role_Hash[i] = Role_Test;
+                }
+                else 
+                {
+                    //found role does not exist
+                    role = false;
+                }
             }
         }       
         
@@ -406,26 +395,14 @@ contract AM_SC_RBAC is AccessControl//, Ownable
             Users_Info[User_Add].Monitor_Behavior.Prev_Request_Fail = false; //set the user previous request was successful
         }
 
-
         Generate_RBAC_Session(System_Hash, User_Add, Role_Hash); //Generate session for user to access system
-
-
         Access = true;  //decide the user can access the system
         return Access;  //User can access the system
-
-           
-
     }
 
-    //evaluate the user access to the system, takes the user address
+    //evaluate the user access to the system, takes the user address and the system requested
     function Access_Request_Deny(address User_Add, string memory Requested_System) private
     {
-        uint256 Descision_Time = block.timestamp;   //get the current time from the block
-        uint256 Last_Fail_Time = Users_Info[User_Add].Monitor_Behavior.Last_Request_Time;   //fetch last time the user had a failed request
-        uint256 Time_Diff = Descision_Time - Last_Fail_Time;    //calculate the times between requests
-
-        Time_Diff = Time_Diff / 1 hours;    // compute the time in hours
-
         if (Users_Info[User_Add].Monitor_Behavior.Request_Deny_Count > 2)   //if a user has had 3 failed system requests
         {
             //restrict the user
@@ -438,7 +415,6 @@ contract AM_SC_RBAC is AccessControl//, Ownable
             Users_Info[User_Add].Monitor_Behavior.Last_Request_Time = block.timestamp;  //  store the time at which the user attempted the request
             Users_Info[User_Add].Monitor_Behavior.Prev_Request_Fail = true; //  set the previous request failiure to true
         }
- 
     }
 
 
@@ -450,7 +426,14 @@ contract AM_SC_RBAC is AccessControl//, Ownable
         string memory Role_String = Create_User_Role_String(User_Add);  // create user role string to be used in logging
         string memory System_Role_String = Create_System_Role_String(Requested_System); //  create system role string to be used in logging
 
-        string memory Abuse = string(abi.encodePacked(Strings.toString(block.timestamp),",",Strings.toHexString(uint256(uint160(User_Add)), 20),",",Role_String,",",System_Role_String,",",Requested_System,",",Users_Info[User_Add].Monitor_Behavior.Last_Requested_Resource,",",Strings.toString(Users_Info[User_Add].Monitor_Behavior.User_Suspended_Count),";")); //generate log for SOC team
+        string memory Abuse = string(abi.encodePacked(
+            Strings.toString(block.timestamp),",",
+            Strings.toHexString(uint256(uint160(User_Add)), 20),",",
+            Role_String,",",
+            System_Role_String,",",
+            Requested_System,",",
+            Users_Info[User_Add].Monitor_Behavior.Last_Requested_Resource,",",
+            Strings.toString(Users_Info[User_Add].Monitor_Behavior.User_Suspended_Count),";")); //generate log for SOC team
         Abuse_Log.push(Abuse);  // store log
     }
 
@@ -492,7 +475,8 @@ contract AM_SC_RBAC is AccessControl//, Ownable
 
     /**********************************RBAC Sessions***************************************/
 
-    //Create an RBAC session when a user is authenticated against a system, takes the hash of the system being accessed, the address of the user, and the hash of the role being used within the sesison
+    //Create an RBAC session when a user is authenticated against a system, takes the hash of the system being accessed, 
+    //the address of the user, and the hash of the role being used within the sesison
     function Generate_RBAC_Session(bytes32 System_Hash, address User_Add, bytes32[] memory Role_Hash) private
     {
         require(User_Sessions[User_Add].Session_Init_Time == 0, "Session Already exists for this user");
@@ -506,7 +490,7 @@ contract AM_SC_RBAC is AccessControl//, Ownable
         Index_Session++;    //  increment number of active sessions
     }
 
-
+    //Terminate the RBAC session generated by the user, takes no arguments
     function Terminate_RBAC_Session() public
     {
         address User_Add = msg.sender;
@@ -514,45 +498,44 @@ contract AM_SC_RBAC is AccessControl//, Ownable
 
         delete User_Sessions[User_Add];
 
-        //Index_Session--;
+        Index_Session--;
 
     }
 
+    //Request access to a system resource, takes the resource to be accessed and the permissions to access the resource with
     function Resource_Access(string memory Resource, uint256 Requested_Permissions) public returns (bool Access)
     {
         address User_Add = msg.sender;
         require(User_Sessions[User_Add].Session_Init_Time != 0, "No session for this user");    //  ensure user has a sesison assigned to them
-        
-
         bytes32 Requested_Resource_Hash = keccak256(abi.encodePacked(Resource));    //  generate hash of the requirested resource
         require(Systems[User_Sessions[User_Add].System_Hash].System_Resources[Requested_Resource_Hash].Resource_Timestamp != 0,"Requested Resource Does Not Exist On This System"); // ensure resource exists on the system
-
         bytes32 Session_Hash_Role;
 
         for (uint256 i = 0; i<User_Sessions[User_Add].Session_Role_Hash.length; i++) //for the number of roles attributed to the session
         {
             Session_Hash_Role = User_Sessions[User_Add].Session_Role_Hash[i];
-
-            if (RBAC_Roles[Session_Hash_Role].Allowed_Resources[Requested_Resource_Hash].Resource_Timestamp != 0)   // if the requested resource is allowed by the RBAC role
-            {
-                if ((Requested_Permissions == 1) && (RBAC_Roles[Session_Hash_Role].Allowed_Resources[Requested_Resource_Hash].Resource_Permissions.Read))   // if the permisions of the rbac role allow the user to perform read against this resource AND the user requests to read the resource
+            // if the requested resource is allowed by the RBAC role
+            if (RBAC_Roles[Session_Hash_Role].Allowed_Resources[Requested_Resource_Hash].Resource_Timestamp != 0)   
+            {   
+                // if the permisions of the rbac role allow the user to perform read against this resource AND the user requests to read the resource
+                if ((Requested_Permissions == 1) && (RBAC_Roles[Session_Hash_Role].Allowed_Resources[Requested_Resource_Hash].Resource_Permissions.Read))   
                 {
                     return true;    //  enable the user to read the resource
                 }
-                else if ((Requested_Permissions == 2) && (RBAC_Roles[Session_Hash_Role].Allowed_Resources[Requested_Resource_Hash].Resource_Permissions.Write)) // if the permisions of the rbac role allow the user to perform write against this resource AND the user requests to write to the resource
+                // if the permisions of the rbac role allow the user to perform write against this resource AND the user requests to write to the resource
+                else if ((Requested_Permissions == 2) && (RBAC_Roles[Session_Hash_Role].Allowed_Resources[Requested_Resource_Hash].Resource_Permissions.Write)) 
                 {
                     return true;    // enable the user to write to the resource
                 }
-                else if ((Requested_Permissions == 3) && (RBAC_Roles[Session_Hash_Role].Allowed_Resources[Requested_Resource_Hash].Resource_Permissions.Execute)) // if the permisions of the rbac role allow the user to perform executes against this resource AND the user requests to execute the resource
+                // if the permisions of the rbac role allow the user to perform executes against this resource AND the user requests to execute the resource
+                else if ((Requested_Permissions == 3) && (RBAC_Roles[Session_Hash_Role].Allowed_Resources[Requested_Resource_Hash].Resource_Permissions.Execute)) 
                 {
                     return true;    //  enable the user to execute the resource
                 }
             }
 
         }
-
         //after every role is checked for the required permissions, and they are unsucessful, begin user monitoring 
-
         Users_Info[User_Add].Monitor_Behavior.Request_Deny_Count++; //  increment the number of denyied requests
         Users_Info[User_Add].Monitor_Behavior.Last_Requested_Resource = Systems[User_Sessions[User_Add].System_Hash].System_Resources[Requested_Resource_Hash].Resource_Name;
         Users_Info[User_Add].Monitor_Behavior.Resource_Request_Fail = true;
@@ -571,19 +554,20 @@ contract AM_SC_RBAC is AccessControl//, Ownable
 
     /*----------------------------SOC Team Functionality---------------------------------*/
 
-    //set up the Security Operations Centre account, take the address of the SOC account
+    //set up the Security Operations Centre account, takes the address of the SOC account
     function Set_Up_SOC(address SOC_Add) public
     {
         require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender));   //ensure only the admin can assign a SOC user
         grantRole(SOC_Role, SOC_Add);                       //grant the account the role of SOC
     }
 
+    //remove a Security Operations Centre account, takes the address of the SOC account to remove
     function Remove_SOC(address SOC_Add) public
     {
         require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender));
         require(hasRole(SOC_Role, SOC_Add));
 
-        renounceRole(SOC_Role, SOC_Add);
+        revokeRole(SOC_Role, SOC_Add);
     }
 
     //return the logs generated by the access monitoring
